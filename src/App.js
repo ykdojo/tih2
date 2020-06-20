79318c4cbf992b6e249b63d6879e8843a53100af
ykdojo <yksu4p@gmail.com>
2020-06-19 19:16:07 -0700

    Added a navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import "bootswatch/dist/darkly/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
   return (
     <div className="App">
-      This is the content with in App.
+      <nav className="navbar navbar-dark bg-primary navbar-expand-sm fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="/">TechInterviewHub.io</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="navbar-item nav-link">
+                <a href="/">Questions</a>
+              </li>
+              <li className="navbar-item nav-link">
+                <a href="/">Submit</a>
+              </li>
+              <li className="navbar-item nav-link">
+                <a href="/">About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
