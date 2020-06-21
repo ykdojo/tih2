498887008bca37ee6ca675cedb202d6c92d75cf8
ykdojo <yksu4p@gmail.com>
2020-06-20 18:37:05 -0700

    Made the QuestionCard component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import QuestionCard from './components/question-card';
 import './App.css';
 import "bootswatch/dist/darkly/bootstrap.min.css";
 import 'bootstrap/dist/js/bootstrap.js';
@@ -30,17 +31,10 @@ function App() {
 
       <div className="container" style={{marginTop: '90px'}}>
         <h4>Recent interview questions</h4>
-        <div className="card">
-          <div className="card-body">
-            <h4 className="card-title">Macrohard - Software Engineer</h4>
-            <p className="card-text" style={{whiteSpace: 'break-spaces'}}>{`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-
-Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`}</p>
-            <h6 className="card-subtitle text-muted">Posted by Anonymous</h6>
-          </div>
-        </div>
+        <QuestionCard company='Macrohard' position='Software Engineer'/>
+        <QuestionCard company='Banana' position='Software Engineer'/>
+        <QuestionCard company='Poodle' position='Product Manager'/>
+        <QuestionCard company='Bookface' position='Software Engineer'/>
       </div>
     </div>
   );
