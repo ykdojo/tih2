df7e66df94b5cfe73c37c5a2184b3121dfa41a73
ykdojo <yksu4p@gmail.com>
2020-06-22 21:27:05 -0700

    The question form works

--- src/components/question-form.js
+++ src/components/question-form.js
@@ -3,25 +3,45 @@ import React from 'react';
 class QuestionForm extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      company: '',
+      position: '',
+      question: '',
+      isAnonymous: true,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.preventDefault();
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.name === 'isAnonymous' ? target.checked : target.value;
+    this.setState({[target.name]: value});
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div className="form-group">
           <label>Company</label>
-          <input type="text" name="company" className="form-control" placeholder="Which company gave this question?"/>
+          <input type="text" name="company" value={this.state.company} onChange={this.handleChange} className="form-control" placeholder="Which company gave this question?"/>
         </div>
         <div className="form-group">
           <label>Position</label>
-          <input type="text" name="position" className="form-control" placeholder="Which position was it for?"/>
+          <input type="text" name="position" value={this.state.position} onChange={this.handleChange} className="form-control" placeholder="Which position was it for?"/>
         </div>
         <div className="form-group">
           <label>Question</label>
-          <textarea name="question" className="form-control" placeholder="Please describe the question in detail." rows="7"/>
+          <textarea name="question" value={this.state.question} onChange={this.handleChange} className="form-control" placeholder="Please describe the question in detail." rows="7"/>
         </div>
         <div className="form-check">
-          <input type="checkbox" name="isAnonymous" className="form-check-input"/>
+          <input type="checkbox" name="isAnonymous" checked={this.state.isAnonymous} onChange={this.handleChange} className="form-check-input"/>
           <label className="form-check-label">Submit Anonymously</label>
         </div>
         <div className="mt-3">
